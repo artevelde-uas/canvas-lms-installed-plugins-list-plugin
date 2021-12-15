c2efc2b80217080b441d1a425c7dbe86e2a5bc11
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2021-12-15 22:25:19 +0100

    Import plug-in data from main app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import { router, dom } from '@artevelde-uas/canvas-lms-app';
+import { getPluginData } from '@artevelde-uas/canvas-lms-app';
 import ReactDOM from 'react-dom';
 
 import PluginTable from './components/PluginTable';
 
 
-export default function ({ plugins }) {
+export default function () {
+    const plugins = getPluginData();
+
+    // Don't do anything if no plugins were installed
     if (plugins.size === 0) return;
+
+    const pluginData = plugins.map(value => value.info);
 
     router.onRoute('profile.settings', async params => {
         // Wait for the feature flag element
@@ -23,6 +29,6 @@ export default function ({ plugins }) {
         `);
 
         // Render the component
-        ReactDOM.render(<PluginTable plugins={plugins} />, container);
+        ReactDOM.render(<PluginTable plugins={pluginData} />, container);
     });
 }
